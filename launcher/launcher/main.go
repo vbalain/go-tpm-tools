2f29aacbe747472e9bfc9db0cac0a026c6c028df
vbalain <vbalain@google.com>
2024-03-05 06:00:47 +0000

    wg changes

--- launcher/launcher/main.go
+++ launcher/launcher/main.go
@@ -268,7 +268,59 @@ func startLauncher(launchSpec spec.LaunchSpec, serialConsole *os.File) error {
 		return fmt.Errorf("failed to get updated device: %v", err)
 	}
 
+	printDevice(device)
+
+	for _, p := range device.Peers {
+		printPeer(p)
+	}
+
 	return r.Run(ctx)
+}
+
+func printDevice(d *wgtypes.Device) {
+	const f = `interface: %s (%s)
+  public key: %s
+  private key: (hidden)
+  listening port: %d
+
+`
+
+	fmt.Printf(
+		f,
+		d.Name,
+		d.Type.String(),
+		d.PublicKey.String(),
+		d.ListenPort)
+}
+
+func printPeer(p wgtypes.Peer) {
+	const f = `peer: %s
+  endpoint: %s
+  allowed ips: %s
+  latest handshake: %s
+  transfer: %d B received, %d B sent
+
+`
+
+	fmt.Printf(
+		f,
+		p.PublicKey.String(),
+		// TODO(mdlayher): get right endpoint with getnameinfo.
+		p.Endpoint.String(),
+		ipsString(p.AllowedIPs),
+		p.LastHandshakeTime.String(),
+		p.ReceiveBytes,
+		p.TransmitBytes,
+	)
+}
+
+func ipsString(ipns []net.IPNet) string {
+	ss := make([]string, 0, len(ipns))
+	for _, ipn := range ipns {
+		ss = append(ss, ipn.String())
+	}
+
+	return strings.Join(ss, ", ")
 }
 
 // verifyFsAndMount checks the partitions/mounts are as expected, based on the command output reported by OS.
