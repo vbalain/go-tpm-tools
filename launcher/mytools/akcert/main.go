c2519cf2eb25e7d6525a0d1d6367a1687fda8d6c
vbalain <vbalain@google.com>
2024-03-19 10:56:03 +0000

    change

--- launcher/mytools/akcert/main.go
+++ launcher/mytools/akcert/main.go
@@ -8,27 +8,24 @@ import (
 )
 
 func main() {
-	fmt.Println("vaibhav 0 ")
 	tpm, err := tpm2.OpenTPM("/dev/tpmrm0")
 	if err != nil {
-		fmt.Println("vaibhav 1 ", err)
+		fmt.Println(err)
 		return
 	}
-	fmt.Println("vaibhav 2 ", tpm)
 	defer tpm.Close()
 
 	// check AK (EK signing) cert
 	gceAk, err := client.GceAttestationKeyECC(tpm)
 	if err != nil {
-		fmt.Println("vaibhav 3 ", err)
+		fmt.Println(err)
 		return
 	}
-	fmt.Println("vaibhav 4 ", gceAk)
+	fmt.Println(gceAk)
 	if gceAk.Cert() == nil {
-		fmt.Println("vaibhav 5 failed to find AKCert on this VM: try creating a new VM or contacting support")
+		fmt.Println("failed to find AKCert on this VM: try creating a new VM or contacting support")
 		return
 	}
-	fmt.Println("vaibhav 6 ", gceAk.Cert())
+	fmt.Println(gceAk.Cert())
 	gceAk.Close()
-	fmt.Println("vaibhav 7")
 }
